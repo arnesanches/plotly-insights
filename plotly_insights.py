e855339ba8fb9bc21a6dda89249e82fcae60dbcb
arnesanches <arneantonio@hotmail.com>
2024-12-08 21:45:13 -0300

    Remover coluna desnecessária

--- plotly_insights.py
+++ plotly_insights.py
@@ -2,3 +2,7 @@ import pandas as pd  # Nome comum para pandas
 
 # Leitura do arquivo CSV com dados de cancelamentos
 tabela = pd.read_csv("cancelamentos_sample.csv")
+
+# Remover a coluna "CustomerID" que não é relevante para a análise
+tabela = tabela.drop(columns="CustomerID")
+print(tabela)
