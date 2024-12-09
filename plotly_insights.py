9548a39bd9eaeed3cf97baae9496a580378fee7a
arnesanches <arneantonio@hotmail.com>
2024-12-08 21:48:11 -0300

    Remover valores ausentes

--- plotly_insights.py
+++ plotly_insights.py
@@ -6,3 +6,10 @@ tabela = pd.read_csv("cancelamentos_sample.csv")
 # Remover a coluna "CustomerID" que não é relevante para a análise
 tabela = tabela.drop(columns="CustomerID")
 print(tabela)
+# Exibir informações sobre a tabela, como tipos de dados e valores ausentes
+print(tabela.info())
+
+# Remover linhas com valores ausentes para garantir integridade dos dados
+tabela = tabela.dropna()
+print(tabela.info())
+
